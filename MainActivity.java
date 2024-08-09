940f9b7841c5f39fe202de877cc6e51785b91f6a
kaoru2209 <jaaaaan.k@gmail.com>
2024-08-09 11:22:19 +0900

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.myfragment;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
